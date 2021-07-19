31a262282b12b7f26457524538ddc86e281b1f9c
sbn073 <Scott.B.Normore@gmail.com>
2021-07-19 16:08:34 -0600

    First Commit/Creation
    Linked List for assignment 3 of studies.

---
+++ CPRG251A5Skeleton/src/sait/sll/utility/Node.java
@@ -0,0 +1,40 @@
+package sait.sll.utility;
+
+public class Node {
+	private Object element;
+	private Node nextNode;
+	
+	public Node(Object element, Node nextNode) {
+		super();
+		this.element = element;
+		this.nextNode = nextNode;
+	}
+	
+	public Node(Object element) {
+		super();
+		this.element = element;
+		this.nextNode = null;
+	}
+
+	public Object getElement() {
+		return element;
+	}
+
+	public void setElement(Object element) {
+		this.element = element;
+	}
+
+	public Node getNext() {
+		return nextNode;
+	}
+
+	public void setNext(Node nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	/*
+	public String toString() {
+		return this.element.toString();
+	}
+	*/
+}
